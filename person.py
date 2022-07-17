5b5583b0291c5e7b9ed561ec1b93087780116b66
Kaali <kaali1479@gmail.com>
2022-07-17 18:27:09 +0530

    write in json file funcation is working

--- person.py
+++ person.py
@@ -2,37 +2,61 @@ import json
 
 #this is question for ask to person
 detail = [
+    "ID",
     "Full Name",
     "Nick Name",
     "Age",
     "Phone No",
     "Address",
     "Work",
-    "ID",
-    "Password",
+    "Insta I'd",
+    "Insta Password",
     "About",
 ]
 
-#this is save a data
-json_file = open("infro.json", "a")
+#what u want write a new person detail and search old person detail
+print("What u want ? write a new person detail and read old person detail\n")
 
-data = '{}'
+print("write for \'w\' or read for \'r\' \n")
+user = input(">> ")
 
-json_data = json.loads(data)
+if user=="w":
 
-for d in detail:
+    json_file_R = open("infro.json", "r")
+    json_data = json.load(json_file_R)         
+
+    json_file_W = open("infro.json", "w")
+
+    data = {}                              
+
+    ID = ""
+
+    for d in detail:
+        if d=="ID":
+            print("Next person don't get your detail\n")
+            print("Frist create our ID\n")
+            ID = input(f"{d} >> ")
+
+        else:
+            user = input(f"{d} >> ")
+            new = {f"{d}":f"{user}"}
+            data.update(new)
+
+
+    person = {f"{ID}": data}       
+
+    json_data.update(person)
+
+    json.dump(json_data, json_file_W, indent=4)
     
-    user = input(f"{d} >> ")
-    new = {f"{d}":f"{user}"}
-    json_data.update(new)
+    json_file_W.close()
+    json_file_R.close()
+    
 
-ID = json_data["ID"]
+elif user=="r":
+    print("reading data.......")
 
-person = {
-    f"{ID}": json_data
-}
+else:
+    print("plz! enter proporly again.")
 
-json_file.write(json.dumps(person))
-
-json_file.close()
 
